9fe20f10d57dc64f8ec97c5a491d90b9baf8934c
Rafael Tavares <rafael.tawares@gmail.com>
2025-09-19 17:41:49 -0300

    chore: update Storybook to latest version

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,13 @@
 const config = {
   staticDirs: ['../public'],
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-docs'],
+
   framework: {
     name: '@storybook/nextjs',
     options: {}
   },
-  docs: {
-    autodocs: true
-  },
+
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
